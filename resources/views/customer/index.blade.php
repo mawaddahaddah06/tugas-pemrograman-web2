9c226795d661bc45dd5e1ada6c0af94e920a1108
mawaddah <mawaddah24@mhs.unitama.ac.adgit config --global user.email mawaddah@mhs.unitama.ac.id>
2026-06-03 13:44:01 +0700

    Database Setup & Relasi: pembuatan migration, model dengan relasi, seeder, dan factory untuk Member dan Customer

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -8,16 +8,16 @@
             <li class="list-group-item">
                 {{ $loop->iteration }}. {{ $customer->name }} -- {{ $customer->email }} --
                 {{ $customer->phone }} -- {{ $customer->address }} -- {{ $customer->birthdate }}
+
+                <a class="btn btn-warning btn-sm" href="{{ route('customer.edit', $customer) }}" role="button">Edit</a>
+                <form action="{{ route('customer.destroy', $customer) }}" method="POST" class="d-inline">
+                    @method('DELETE')
+                    @csrf
+
+                    <button type="submit" class="btn btn-danger btn-sm"
+                        onclick="return confirm('Anda Yakin')">Delete</button>
+                </form>
             </li>
-            <a class="btn btn-warning btn-sm" href="{{ route('customer.edit', $customer) }}" role="button">Edit</a>
-            <form action="{{ route('customer.destroy', $customer) }}" method="POST" class="d-inline">
-                @method('DELETE')
-                @csrf
-
-
-                <button type="submit" class="btn btn-danger btn-sm"
-                    onclick="return confirm('Anda Yakin')">Delete</button>
-            </form>
         @endforeach
     </ul>
 </x-app>
